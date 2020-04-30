f0f833899d38ae7425926042e025f83f41a53e7a
londhemayuri <60913701+londhemayuri@users.noreply.github.com>
2020-04-30 14:40:46 +0530

    new upadated

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,160 +1,63 @@
-import 'src/styles/index.scss';
-import { getData } from 'E:\news-aggregator-app\src\scripts\request.js';
+//for headlines
+  var url = 'https://newsapi.org/v2/top-headlines?'+
+            'country=in&'+
+            'apiKey=8857ddfa72ae47238500738f55cc70f1'; 
+            let httpGetAsync = (theUrl, callback) => {
+            // AJAX
+            var xmlHttp = new XMLHttpRequest();
 
-const apiKey = process.env.APIKEY;
-const headlinesURL = `http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=d11a43d85241423d80a4a8afed8021c7`;
-const everythingURL = `http://newsapi.org/v2/everything?q=bitcoin&from=2020-03-28&sortBy=publishedAt&apiKey=d11a43d85241423d80a4a8afed8021c7`;
+            // function which executes when state of your request changes
+            xmlHttp.onreadystatechange = () => {
+                if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+                    // this is the important line
+                    callback(xmlHttp.responseText);
+                }
+            }
 
-// elements
-
-const loader = document.querySelector('#loading');
-const newsContainer = document.querySelector('#news-articles');
-const more = document.querySelector('#show-more');
-const searchBox = document.querySelector('#search');
-const notFound = document.querySelector('.not-found');
-const error = document.querySelector('.error');
-const clearSearch = document.querySelector('#clear-search');
-const switcher = document.querySelector('.mode-switch');
-
-// data
-
-let articles = [];
-let totalResults = 0;
-let state = 'h';
-const headlinesFilter = {
-    page: 1,
-    pageSize: 20,
-    q: ''
-};
-let theme;
-
-init();
-
-async function getArticles(
-    isSearch,
-    page= headlinesFilter.page,
-    pageSize= headlinesFilter.pageSize,
-    q = headlinesFilter.q
-
-){
-    try{
-        const url = isSearch
-        ? `${everythingURL}&q=${q}&page=${page}&pageSize=${pageSize}`
-        : `${headlinesURL}&page=${page}&pageSize=${pageSize}`;
-        state = isSearch ? 'e' : 'h' ;
-
-        const data = await getData(url);
-
-        if (data.status !== 'ok'){
-            handleError(error);
-            return;
+            xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+            xmlHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            xmlHttp.send(null);
         }
 
-        if (data.articles.length === 0){
-            notFound.style.display = 'block' ;
-            newsContainer.style.display = 'none' ;
-            articles = [];
-            return;
+        let nodeCreated = `
+        <li class="article">
+              <img class="article-img" src="IMG-20180709-WA0015.jpg" alt="image" style="width:100%">
+              <h2 class="article-title"> hello hii</h2>
+              <p class="article-description">hxjhsgyghwyugyuvyux6fwxgfw</p>
+              <span class="article-author" style="display: block;"> shaikh.a.a</span>
+              <a class="article-link" href="google.com"> hhjh</a>
+          </li>
+
+        `;
+
+        let output = "";
+
+        let makeSomeHTML = (response) => {
+            let obj = JSON.parse(response);
+            let dataArr = obj["articles"];
+              for (let i = 0; i < dataArr.length; i++) {
+                  let currObj = dataArr[i];
+                  let atitle = currObj["title"];
+                  let aauthor = currObj["author"];
+                  let adescription = currObj["description"];
+                  let aimage = currObj["urlToImage"];
+                  let alink = currObj["url"];
+                  let outTemplate = `
+                         <li class="article">
+                              <img  class="article-img" src="${aimage}" alt="image" style="width:100%" ><br><br>
+                              <h2 class="article-title"> ${atitle}</h2><br>
+                              <p class="article-description">${adescription}</p><br>
+                              <span class="article-author" style="display: block;"> ${aauthor}</span><br>
+                              <a class="article-link" href="${alink}">link to page </a>
+                        </li>    
+                    `;
+                output = output+ outTemplate;
+                
+              
+            }
+            document.querySelector('#news-articles').innerHTML = output;
         }
+        httpGetAsync(url, makeSomeHTML);
 
-        // show data on error
-        totalResults = data.totalResults;
-        articles = [...articles, ...data.articles];
 
-        const list = renderNews(articles);
-
-        newsContainer.innerHTML = list;
-        console.log(list);
-        newsContainer.style.display = 'grid';
-        notFound.style.display = 'none';
-
-    }
-
-    catch (error){
-        handleError(error);
-    }
-
-    finally {
-        loader.style.display = 'none';
-        if (totalResults !== articles.length && articles.length > 0){
-            more.style.display = 'block';
-        }
-        else {
-            more.style.display = 'none';
-        }
-    }
-}
-
-function init() {
-    //themeInit();
-    loader.style.display = 'flex' ; 
-    error.style.display = 'none';
-    clearSearch.style.display = 'none';
-    articles = [];
-    getArticles(false);
-}
-
-function clear() {
-    searchBox.value = null ;
-    clearSearch.style.display = 'none';
-    init();
-}
-
-function handleError(msg = null){
-    error.style.display = 'block';
-}
-
-function renderNews(news) {
-    let html = ' ';
-    news.forEach(n => {
-        const author = n.author ? n.author : n.source.name;
-        const image = n.urlToImage ? n.urlToImage
-        : 'https://via.placeholder.com/150';
-        html += `<li class="article">
-        <a href="${n.url}" class="article-link">
-            <div class="article__head">
-                <img
-                    src="${image}"
-                    alt="${n.title}"
-                    class="article-img"
-                />
-                <h2 class="article-title">
-                ${n.title}
-                </h2>
-            </div>
-            <p class="article-description">
-                ${n.description ? n.description : ''}
-            </p>
-            
-            <span id="article-author" class="article-author">${author}<span>
-        </a>
-    </li>`;
-
-    });
-    
-    
-    return  html;
-}
-
-searchBox.addEventListener('keyup', e => {
-    clearSearch.style.display = 'block' ;
-    if (e.keyCode === 13 ) {
-        loader.style.display = 'flex' ; 
-        newsContainer.style.display = 'none' ;
-        headlinesFilter.q = e.target.value.trim();
-        articles = [];
-        getArticles(true);
-    } 
-});
-
-clearSearch.addEventListener('click',clear);
-
-more.addEventListener('click', infiniteScroll);
-
-function infiniteScroll(){
-    loader.style.display = 'flex' ;
-    headlinesFilter.page++;
-    const isSearch = state === 'e' ? true : false ;
-    getArticles(isSearch);
-}
 
